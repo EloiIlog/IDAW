e98e5f5cfdea23c502efdbe580faf69389cb8dec
Eloi Guisnet <77975271+EloiIlog@users.noreply.github.com>
2021-03-18 15:09:48 +0100

    exo2 connected

--- TP3/connected.php
+++ TP3/connected.php
@@ -1,25 +1,51 @@
     <?php
         // on simule une base de données
-        $users = array(
+        //$users = array(
         // login => password
-        'riri' => 'fifi',
+        /*'riri' => 'fifi',
         'yoda' => 'maitrejedi',
-        'eloi' => 'lebest' );
+        'eloi' => 'lebest' );*/
         $login = "anonymous";
+
+        //database connexion
+            $servername = 'localhost';
+            $username = 'root';
+            $password = '';
+            $database='tp3';
+            
+            //On établit la connexion
+            $conn = new mysqli($servername, $username, $password,$database);
+            
+            //On vérifie la connexion
+            if($conn->connect_error){
+                die('Erreur : ' .$conn->connect_error);
+            }
+            echo 'Connexion database reussi';
+            echo "<br>";
+
+            $sql = $conn->query("SELECT * from login");
+        
         $errorText = "";
         $successfullyLogged = false;
         if(isset($_POST['login']) && isset($_POST['password'])) {
         $tryLogin=$_POST['login'];
         $tryPwd=$_POST['password'];
+        
 
         // si login existe et password correspond
-            if( array_key_exists($tryLogin,$users) && $users[$tryLogin]==$tryPwd ) {
-            $successfullyLogged = true;
-            $login = $tryLogin;
-            } else
-            $errorText = "Erreur de login/password";
-        } else
+            while($row = $sql->fetch_assoc()){ 
+                if($row['Login']==$tryLogin && $row['Password']==$tryPwd){
+                $successfullyLogged = true;
+                $login = $tryLogin;
+                } 
+                else{
+                $errorText = "Erreur de login/password";
+                }
+            }
+        } 
+        else
         $errorText = "Merci d'utiliser le formulaire de login";
+        
         if(!$successfullyLogged) {
         echo $errorText;
         } else {
